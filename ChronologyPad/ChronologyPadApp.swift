b05fe446d2229c05b82a2591815252d9cad0f872
milknuts <milknuts@mac.com>
2026-06-02 10:53:06 +0900

    Initial Commit

---
+++ ChronologyPad/ChronologyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronologyPadApp.swift
+//  ChronologyPad
+//
+//  Created by 小西克尚 on 2026/06/02.
+//
+
+import SwiftUI
+
+@main
+struct ChronologyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronologyPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChronologyPad
+//
+//  Created by 小西克尚 on 2026/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
